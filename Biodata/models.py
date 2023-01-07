7e6d5b2c4faf92e7d3942b18ff6ebd6cb40e6bb1
geky <gyabaayeboahemmanul@gmail.com>
2023-01-07 01:12:10 +0000

    I have started with the applant detials models

--- Biodata/models.py
+++ Biodata/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+TITLE_CHOICE = (
+ ("Mr.", "Mr."),
+ ("Mrs.", "Mrs."),
+ ("Ms.", "Ms"),
+ ("Rev.", "Rev."),
+ ("Sr.", "Sr.")
+)
+
+GENDER_CHOICE = (
+    ("Male", "Male"),
+    ("Female", "Female"),
+)
+
+class ApplicantDetail(models.Model):
+    Title = models.CharField(choices= TITLE_CHOICE, max_length=20)
+    Surname = models.CharField(max_length=255, verbose_name="Surname")
+    FirstName = models.CharField(max_length=255, verbose_name= "First Name")
+    MiddleName = models.CharField(max_length=255, blank=True,null=True,verbose_name="Middle Name" )
+    DateofBirth = models.DateField(verbose_name="Date of Birth")
+    Gender = models.CharField(choices = GENDER_CHOICE, max_length=20)
+    
+
+    def __str__(self) -> str:
+        return self.Surname + " " +self.FirstName 
