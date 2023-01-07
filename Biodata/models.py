deab2dedb3d5d2f4d7edb9f5c40cb88bdbf2f191
Gyedu Samuel <samuelgyedu349@gmail.com>
2023-01-07 01:59:34 +0000

    Applicant details updated

--- Biodata/models.py
+++ Biodata/models.py
@@ -15,6 +15,13 @@ GENDER_CHOICE = (
     ("Female", "Female"),
 )
 
+NATIONALITY_CHOICE =(
+    ("Ghanaian","Ghanaian" ),
+    ("Nigerian", "Nigerian"),
+    ("Benin", "Benin"),
+    ("Garbon",  "Garbon"),    
+    )
+
 class ApplicantDetail(models.Model):
     Title = models.CharField(choices= TITLE_CHOICE, max_length=20)
     Surname = models.CharField(max_length=255, verbose_name="Surname")
@@ -22,7 +29,12 @@ class ApplicantDetail(models.Model):
     MiddleName = models.CharField(max_length=255, blank=True,null=True,verbose_name="Middle Name" )
     DateofBirth = models.DateField(verbose_name="Date of Birth")
     Gender = models.CharField(choices = GENDER_CHOICE, max_length=20)
-    PlaceofBirth=models.CharField(max_length=30, verbose_name="Place of Birth")
+    PlaceofBirth = models.CharField(max_length=30, verbose_name="Place of Birth")
+    RegionofBirth = models.CharField(max_length=30, verbose_name="Region of Birth")
+    Hometown = models.CharField(max_length=20, verbose_name="Hometown")
+    RegionofHometown = models.CharField(max_length=30, verbose_name="Region of Hometown")
+    Nationality = models.CharField(max_length=30,choices= NATIONALITY_CHOICE, verbose_name="Nationality")
+
 
     def __str__(self) -> str:
         return self.Surname + " " +self.FirstName 
